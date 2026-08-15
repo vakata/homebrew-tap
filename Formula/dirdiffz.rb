9101e747d5b63b1d13ee6aaef63f88a86edb70a0
Ivan Bozhanov <github@vakata.com>
2026-08-15 13:49:47 +0300

    Update dirdiffz.rb

--- Formula/dirdiffz.rb
+++ Formula/dirdiffz.rb
@@ -5,24 +5,24 @@ class Dirdiffz < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/vakata/dirdiffz/releases/download/v0.1.1/dirdiffz-macos-aarch64.tar.gz"
+      url "https://github.com/vakata/dirdiffz/releases/download/0.1.1/dirdiffz-macos-aarch64.tar.gz"
       sha256 "MACOS_ARM64_SHA256"
     end
 
     on_intel do
-      url "https://github.com/vakata/dirdiffz/releases/download/v0.1.1/dirdiffz-macos-x86_64.tar.gz"
+      url "https://github.com/vakata/dirdiffz/releases/download/0.1.1/dirdiffz-macos-x86_64.tar.gz"
       sha256 "MACOS_X86_64_SHA256"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/vakata/dirdiffz/releases/download/v0.1.1/dirdiffz-linux-aarch64.tar.gz"
+      url "https://github.com/vakata/dirdiffz/releases/download/0.1.1/dirdiffz-linux-aarch64.tar.gz"
       sha256 "LINUX_ARM64_SHA256"
     end
 
     on_intel do
-      url "https://github.com/vakata/dirdiffz/releases/download/v0.1.1/dirdiffz-linux-x86_64.tar.gz"
+      url "https://github.com/vakata/dirdiffz/releases/download/0.1.1/dirdiffz-linux-x86_64.tar.gz"
       sha256 "LINUX_X86_64_SHA256"
     end
   end
